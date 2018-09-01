b3965c62fbde142d7f2a417ce76159bc4d046c25
Glitched Polygons <glitchedpolygons@protonmail.com>
2018-09-01 02:55:04 +0200

    Finished v1.0.2 with fresh docs and migration from .NET Core 2.1 framework to netstandard2.0 (for a class library this is better).

--- MailgunEmailSender/IEmailSender.cs
+++ MailgunEmailSender/IEmailSender.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 namespace GlitchedPolygons.Services.MailgunEmailSender
 {
     /// <summary>
-    /// Interface for sending emails asynchronosuly.
+    /// Interface for sending emails asynchronously.
     /// </summary>
     public interface IEmailSender
     {
